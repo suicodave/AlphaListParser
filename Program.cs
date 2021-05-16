713b15218a6bb2f41756127c09b59b0b5c2a563a
suicodave <suico.dave@gmail.com>
2021-05-16 18:07:21 +0800

    added csv helper library

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AlphaListParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
