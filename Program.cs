4ca8bd481c277bb7a929a014c258b944d708bc74
suicodave <suico.dave@gmail.com>
2021-05-16 17:37:41 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlphaListParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
